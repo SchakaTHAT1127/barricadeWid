8883875b454b40191af871699e406aaa8e33c10d
furk4ttr <121024661+SchakaTHAT1127@users.noreply.github.com>
2026-07-20 23:58:10 +0300

    Add files via upload

--- barricadewid/lua/entities/ent_sandbagwid/init.lua
+++ barricadewid/lua/entities/ent_sandbagwid/init.lua
@@ -4,12 +4,7 @@ AddCSLuaFile("globalwarfare3/gamemode/vgui/cl_logipanel.lua")
 include("globalwarfare3/gamemode/logihandler/sv_logihandler.lua")
 include("shared.lua")
 
-util.AddNetworkString("callClient")
-util.AddNetworkString("crateVector")
-
-local firstDeath = true
-
-local randomSoundsBreak = {
+local sandbagBreak = {
 	"ambient/materials/rock1.wav",
 	"ambient/materials/rock2.wav",
 	"ambient/materials/rock3.wav",
@@ -17,7 +12,7 @@ local randomSoundsBreak = {
 	"ambient/materials/rock5.wav"
 }
 
-local randomSounds = {
+local sandbagFly = {
 	"physics/cardboard/cardboard_box_break1.wav",
 	"physics/cardboard/cardboard_box_break2.wav",
 	"physics/cardboard/cardboard_box_break3.wav",
@@ -29,10 +24,10 @@ local randomSounds = {
 }
 
 function ENT:Initialize()
-	firstDeath = true
-	self:SetMaxHealth(50)
+	self.firstDeath = true
+	self:SetMaxHealth(150)
     self:SetHealth(150)
-
+	math.randomseed(os.time())
     self:SetModel("models/sandbags_line2.mdl")
     self:PhysicsInit(SOLID_VPHYSICS) 
     self:SetMoveType(MOVETYPE_VPHYSICS)
@@ -41,7 +36,7 @@ function ENT:Initialize()
     local phys = self:GetPhysicsObject() 
     if phys:IsValid() then
     	-- setting the mass, more than 200 is sketchy because player cant pick
-        phys:SetMass(700)
+        phys:SetMass(1000)
         phys:Wake()
         phys:EnableGravity(true)
     end
@@ -50,169 +45,59 @@ end
 function ENT:Use(activator, caller) 
 end
 
-function ENT:SekonderDeathselDonusum()
-	self:SekonderDeathSonrasiCikanlar()
-end
-
-function ENT:SekonderDeathSonrasiCikanlar()
-	for seks=1,15 do
-		local prop = ents.Create("prop_physics")
-		if not IsValid(prop) then return end
-		local sandbagModel = "models/sandbag.mdl"
-
-		prop:SetModel(sandbagModel)
-		math.randomseed(CurTime()^seks)
-
-		prop:SetPos(self:GetPos() + Vector(math.random(-100, 100),math.random(-15, 15), math.random(0, 25)))
-		prop:SetAngles(AngleRand())
-		prop:SetRenderMode(1)	   
-
-		local alpha = 255
-		local timerID1 = "FadeProp1_" .. prop:EntIndex()
-		local timerID2 = "FadeProp2_" .. prop:EntIndex()
-
-		local randomSound = table.Random(randomSounds)
-		self:EmitSound(randomSound)	
-
-		prop:Spawn()
-		timer.Create(timerID2, 2, 1, function() 
-			timer.Create(timerID1, 0.05, 51, function() 
-				if IsValid(prop) then
-				    alpha = alpha - 5
-
-				    if alpha < 0 then alpha = 0 end
-				        
-				    prop:SetColor(Color(255, 255, 255, alpha))
-				        
-				    if alpha <= 0 then
-				        prop:Remove()
-				    end
-				else
-				    timer.Remove(timerID1)
-				end
-			end)
-		end)
-		local SelfPos = self:GetPos()
-		local toz = EffectData()
-		local Up = Vector(0, 0, 1)
-		local EffectType = 1
-		toz:SetNormal(Up)
-		toz:SetRadius(EffectType)
-		toz:SetOrigin(SelfPos)
-		toz:SetColor(213,200,30)
-		toz:SetScale(0.7)
-		util.Effect("eff_jack_gmod_smalldustshock", toz, true, true)
-		local randomSesler = table.Random(randomSoundsBreak)
-		self:EmitSound(randomSesler, 90, 100)
-		toz:SetScale(0.4)
-		util.Effect("eff_jack_gmod_ezbuildsmoke", toz, true, true)
-		util.ScreenShake(SelfPos, 10, 10, 1.5, 300)
-	end
-	self:Remove()
-end
-
-function ENT:DeathSonrasiCikanlar()
-	for seks=1,10 do
-		local prop = ents.Create("prop_physics")
-		if not IsValid(prop) then return end
-		local sandbagModel = "models/sandbag.mdl"
-
-		prop:SetModel(sandbagModel)
-		math.randomseed(CurTime()^seks)
-		prop:SetPos(self:GetPos() + Vector(0 + math.random(-8, 8), 0 + math.random(-23, 23), 10))
-		prop:SetAngles(AngleRand())
-		prop:SetRenderMode(1)	   
-
-		local alpha = 255
-		local timerID1 = "FadeProp1_" .. prop:EntIndex()
-		local timerID2 = "FadeProp2_" .. prop:EntIndex()
-
-		local randomSound = table.Random(randomSounds)
-		self:EmitSound(randomSound)
-
-		prop:Spawn()
-		timer.Create(timerID2, 2, 1, function() 
-			timer.Create(timerID1, 0.05, 51, function() 
-				if IsValid(prop) then
-				    alpha = alpha - 5
-
-				    if alpha < 0 then alpha = 0 end
-				        
-				    prop:SetColor(Color(255, 255, 255, alpha))
-				        
-				    if alpha <= 0 then
-				        prop:Remove()
-				    end
-				else
-				    timer.Remove(timerID1)
-				end
-			end)
-		end)
-		local SelfPos = self:GetPos()
-		local toz = EffectData()
-		local Up = Vector(0, 0, 1)
-		local EffectType = 1
-		toz:SetNormal(Up)
-		toz:SetRadius(EffectType)
-		toz:SetOrigin(SelfPos)
-		toz:SetColor(213,200,30)
-		toz:SetScale(1)
-		util.Effect("eff_jack_gmod_smalldustshock", toz, true, true)
-		toz:SetScale(0.4)
-		util.Effect("eff_jack_gmod_ezbuildsmoke", toz, true, true)
-		util.ScreenShake(SelfPos, 10, 10, 1.5, 300)
-		local randomSesler = table.Random(randomSoundsBreak)
-		self:EmitSound(randomSesler, 90, 100)
-	end
-end
-
-function ENT:DeathselDonusum()
-	print("my health is" ..self:Health())
-	self:SetModel("models/anzio_sandbags_destroyed.mdl")
-	self:SetHealth(150)
-	print("my NEW health is" ..self:Health())
-	self:PhysicsInit(SOLID_VPHYSICS) 
-	self:SetMoveType(MOVETYPE_VPHYSICS)
-	self:SetSolid(SOLID_VPHYSICS)
-		   
-	local entOLDpos = self:GetLocalPos()
-	print("my OLD pos" .. tostring(entOLDpos))
-	local entNEWpos = entOLDpos + Vector(0,0,21)
-	self:SetPos(entNEWpos)
-
-	local phys = self:GetPhysicsObject() 
-	if phys:IsValid() then
-		phys:SetMass(700)
-		phys:Wake()
-		phys:EnableGravity(true)
-	end
-	self:DeathSonrasiCikanlar()
-end
-
-function ENT:firstDeathTakeDamage()
-	local prop = ents.Create("prop_physics")
-	if not IsValid(prop) then return end
+function ENT:PropCikar(setPosX, setPosY, setPosZ, totalDestroy)
 	local sandbagModel = "models/sandbag.mdl"
-	prop:SetModel(sandbagModel)
-	math.randomseed(CurTime())
-	prop:SetPos(self:GetPos() + Vector(0 + math.random(-5, 5), 0 + math.random(-60, 60), 50))
-	prop:SetAngles(AngleRand())
-	prop:SetRenderMode(1)	   
+	local prop = ents.Create("prop_physics")
 	local propPos = prop:GetPos()
-
 	local alpha = 255
 	local timerID1 = "FadeProp1_" .. prop:EntIndex()
 	local timerID2 = "FadeProp2_" .. prop:EntIndex()
+	local amiPatladi = totalDestroy
 
-	local randomSound = table.Random(randomSounds)
+	if not IsValid(prop) then return end -- block possible errors
+	
+	prop:SetModel(sandbagModel)
+	
+	prop:SetPos(self:GetPos() + Vector(setPosX, setPosY, setPosZ))
+	prop:SetAngles(AngleRand())
+	prop:SetRenderMode(1)	   
+
+	local randomSound = table.Random(sandbagFly)
 	self:EmitSound(randomSound)
 
-	local naganigi = EffectData()
-	naganigi:SetOrigin(propPos)
-	naganigi:SetColor(213,200,30)
-	naganigi:SetScale(0.2)
-	util.Effect("eff_jack_gmod_ezbuildsmoke", naganigi, true, true)
-	util.ScreenShake(propPos, 3, 3, 0.4, 100)
+	if amiPatladi then
+		print("if amiPatladi then")
+		local propPos = prop:GetPos()
+		local ffrrggh = EffectData()
+		local Up = Vector(0, 0, 1)
+		local EffectType = 1
+		ffrrggh:SetNormal(Up)
+		ffrrggh:SetRadius(EffectType)
+		ffrrggh:SetOrigin(propPos)
+		ffrrggh:SetColor(0,0,0)
+		ffrrggh:SetScale(1.1)
+		util.Effect("eff_jack_gmod_smalldustshock", ffrrggh, true, true)
+		local randomSesler = table.Random(sandbagBreak)
+		self:EmitSound(randomSesler, 90, 100)
+		ffrrggh:SetScale(0.33)
+		util.Effect("eff_jack_gmod_ezbuildsmoke", ffrrggh, true, true)
+		util.ScreenShake(propPos, 10, 10, 1.5, 100)
+	elseif not amiPatladi then
+		print("elseif not amiPatladi then")
+		local propPos = prop:GetPos()
+		local naganigi = EffectData()
+		local Up = Vector(0, 0, 1)
+		local EffectType = 1
+		naganigi:SetNormal(Up)
+		naganigi:SetRadius(EffectType)
+		naganigi:SetOrigin(propPos)
+		naganigi:SetColor(0,0,0)
+		naganigi:SetScale(0.3)
+		util.Effect("eff_jack_gmod_ezbuildsmoke", naganigi, true, true)
+		util.ScreenShake(propPos, 5, 5, 1.5, 120)
+	else
+		print("amiPatladi hatası")
+	end
 
 	prop:Spawn()
 	timer.Create(timerID2, 2, 1, function() 
@@ -231,114 +116,51 @@ function ENT:firstDeathTakeDamage()
 			end
 		end)
 	end)
+
 end
 
-function ENT:sekonderDeathTakeDamage()
-	--prop:SetPos(self:GetPos() + Vector(0 + math.random(-5, 5), 0 + math.random(-60, 60), 50))
-	print("not firstDeath")
-	local prop = ents.Create("prop_physics")
-	if not IsValid(prop) then return end
-
-	local sandbagModel = "models/sandbag.mdl"
-	prop:SetModel(sandbagModel)
-	math.randomseed(CurTime())
-	local tarafBelirleRandom = math.random(1, 2)
-
-	if tarafBelirleRandom == 1 then
-		print("taraf belirle random 1")
-		math.randomseed(CurTime())
-		prop:SetPos(self:GetPos() + Vector(0,math.random(-55, -35),math.random(20, 35)))
-		prop:SetAngles(AngleRand())
-		prop:SetRenderMode(1)	   
-		local propPos = prop:GetPos()
-
-		local alpha = 255
-		local timerID1 = "FadeProp1_" .. prop:EntIndex()
-		local timerID2 = "FadeProp2_" .. prop:EntIndex()
-
-		local randomSound = table.Random(randomSounds)
-		self:EmitSound(randomSound)
-
-		local dakalak = EffectData()
-		dakalak:SetOrigin(propPos)
-		dakalak:SetColor(213,200,30)
-		dakalak:SetScale(0.2)
-		util.Effect("eff_jack_gmod_ezbuildsmoke", dakalak, true, true)
-		util.ScreenShake(propPos, 3, 3, 0.4, 100)
-
-		prop:Spawn()
-		timer.Create(timerID2, 2, 1, function() 
-			timer.Create(timerID1, 0.05, 51, function() 
-				if IsValid(prop) then
-				    alpha = alpha - 5
-				    if alpha < 0 then alpha = 0 end
-				        
-				    prop:SetColor(Color(255, 255, 255, alpha))
-				        
-				    if alpha <= 0 then
-				        prop:Remove()
-				    end
-				else
-				    timer.Remove(timerID1)
-				end
-			end)
-		end)
-	elseif tarafBelirleRandom == 2 then
-		print("taraf belirle random 2")
-		math.randomseed(CurTime())
-		prop:SetPos(self:GetPos() + Vector(0,math.random(55, 35),math.random(20, 35)))
-		prop:SetAngles(AngleRand())
-		prop:SetRenderMode(1)	   
-		local propPos = prop:GetPos()
-
-		local alpha = 255
-		local timerID1 = "FadeProp1_" .. prop:EntIndex()
-		local timerID2 = "FadeProp2_" .. prop:EntIndex()
-
-		local randomSound = table.Random(randomSounds)
-		self:EmitSound(randomSound)
-
-		local budalak = EffectData()
-		budalak:SetOrigin(propPos)
-		budalak:SetColor(213,200,30)
-		budalak:SetScale(0.2)
-		util.Effect("eff_jack_gmod_ezbuildsmoke", budalak, true, true)
-		util.ScreenShake(propPos, 3, 3, 0.4, 100)
-
-		prop:Spawn()
-		timer.Create(timerID2, 2, 1, function() 
-			timer.Create(timerID1, 0.05, 51, function() 
-				if IsValid(prop) then
-				    alpha = alpha - 5
-				    if alpha < 0 then alpha = 0 end
-				        
-				    prop:SetColor(Color(255, 255, 255, alpha))
-				        
-				    if alpha <= 0 then
-				        prop:Remove()
-				    end
-				else
-				    timer.Remove(timerID1)
-				end
-			end)
-		end)
+function ENT:SekonderDeathSonrasiCikanlar()
+	for seks=1,15 do
+		self:PropCikar(0, 0, 30, true)
 	end
+	self:Remove()
+end
+
+function ENT:DeathSonrasiCikanlar()
+	for seks=1,10 do
+		self:PropCikar(0, 0, 30, true)
+	end
+end
+
+function ENT:DeathselDonusum()
+	self:SetModel("models/anzio_sandbags_destroyed.mdl")
+	self:SetHealth(150)
+ 
+	local entOLDpos = self:GetLocalPos()
+	local entNEWpos = entOLDpos + Vector(0,0,21)
+	self:SetPos(entNEWpos)
+
+	self:PhysicsInit(SOLID_VPHYSICS) 
+    self:SetMoveType(MOVETYPE_VPHYSICS)
+    self:SetSolid(SOLID_VPHYSICS)
+
+	self:DeathSonrasiCikanlar()
 end
 
 function ENT:OnTakeDamage( dmginfo )
     local damage = dmginfo:GetDamage()
     self:SetHealth(self:Health() - damage)
     local randomSayi = math.random(1, 5)
-    if randomSayi == 1 and firstDeath then
-		self:firstDeathTakeDamage()
-	elseif randomSayi == 1 and not firstDeath then
-		self:sekonderDeathTakeDamage()
+    if randomSayi == 1 and self.firstDeath then
+		self:PropCikar(0, 0, 30, false)
+	elseif randomSayi == 1 and not self.firstDeath then
+		self:PropCikar(0, 0, 30, false)
 	end
 
-	if self:Health() <= 0 and firstDeath then
+	if self:Health() <= 0 and self.firstDeath then
 		self:DeathselDonusum()
-		firstDeath = false
-	elseif self:Health() <= 0 and not firstDeath then
-		self:SekonderDeathselDonusum()
+		self.firstDeath = false
+	elseif self:Health() <= 0 and not self.firstDeath then
+		self:SekonderDeathSonrasiCikanlar()
 	end
 end
